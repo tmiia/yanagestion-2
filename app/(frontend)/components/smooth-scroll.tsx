84d151cce9872cc26643b6f1658a5c03371c96f9
nehemiatelusma <nehemia.telusma@akqa.com>
2025-12-07 20:59:26 +0100

    feat: lenis added

---
+++ app/(frontend)/components/smooth-scroll.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ReactLenis } from "lenis/react";
+import { ReactNode } from "react";
+
+interface SmoothScrollProps {
+  children: ReactNode;
+}
+
+export function SmoothScroll({ children }: SmoothScrollProps) {
+  return (
+    <ReactLenis
+      root
+      options={{
+        lerp: 0.1,
+        duration: 1.2,
+        smoothWheel: true,
+      }}
+    >
+      {children}
+    </ReactLenis>
+  );
+}
+
--- app/(frontend)/layout.tsx
+++ app/(frontend)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SmoothScroll } from "@/(frontend)/components/smooth-scroll";
 import { Grid } from "@/(frontend)/utils/grid";
 
 const geistSans = Geist({
@@ -28,8 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Grid />
-        {children}
+        <SmoothScroll>
+          <Grid />
+          {children}
+        </SmoothScroll>
       </body>
     </html>
   );
